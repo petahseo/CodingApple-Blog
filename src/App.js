c179a49a0bfe325eaa6d25f857e8e459cfd81a00
petahseo <94450217+petahseo@users.noreply.github.com>
2022-08-24 00:51:03 +0900

    finished up to date

--- src/App.js
+++ src/App.js
@@ -1,16 +1,78 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
+  const [title, setTitle] = useState([
+    "가 코트 추천",
+    "다 코트 추천",
+    "나 코트 추천",
+  ]);
+
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "teal", fontSize: "20px" }}>블로그임</h4>
+        <h4 style={{ color: "teal", fontSize: "20px" }}>React Blog</h4>
       </div>
-      <h4>{post}</h4>
+
+      <button
+        onClick={() => {
+          let sort = [...title];
+          setTitle(sort.sort());
+        }}
+      >
+        가나다순 정렬
+      </button>
+
+      <button
+        onClick={() => {
+          let newTitle = [...title];
+          newTitle[0] = "여자 코트 추천";
+          setTitle(newTitle);
+        }}
+      >
+        제목수정
+      </button>
+
+      <div className="list">
+        <h4>
+          {" "}
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            👍
+          </span>
+          {count}
+        </h4>
+        <p>8월 23일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>8월 23일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>8월 23일 발행</p>
+      </div>
+
+      <Modal />
     </div>
   );
 }
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
 
 export default App;
